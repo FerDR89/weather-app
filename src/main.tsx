9a49c87006a4797e43e2507109fa7bc6b5f3fa17
Fernando de Row <ferdr89dev@gmail.com>
2024-09-20 11:39:44 -0300

    chore: add redux toolkit, configure store and provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import ReactQueryProvider from "./api/reactQueryProvider.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ReactQueryProvider>
-      <App />
-    </ReactQueryProvider>
+    <Provider store={store}>
+      <ReactQueryProvider>
+        <App />
+      </ReactQueryProvider>
+    </Provider>
   </StrictMode>
 );
