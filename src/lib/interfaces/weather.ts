cf51f36ae47415ddbc5adfca33d824b006c91803
Fernando de Row <ferdr89dev@gmail.com>
2024-09-20 11:42:43 -0300

    chore: add location and weather interfaces

---
+++ src/lib/interfaces/weather.ts
@@ -0,0 +1,10 @@
+export interface IWeather {
+  day: number;
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  humidity: number;
+  description: string;
+  icon: string;
+}
