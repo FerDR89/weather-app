2d471efff6e45ac0eda416f2915b3b456ca1f8c4
Fernando de Row <ferdr89dev@gmail.com>
2024-09-23 16:18:46 -0300

    feat: add useCurrentWeather hook

---
+++ src/lib/hooks/useCurrentWeather.ts
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchCurrentDay } from "../../api/fetchers";
+import { useAppSelector } from "./useAppSelector";
+import { selectLocation } from "../reducers/locationSlice";
+import { IWeather } from "../interfaces/weather";
+import { weatherTodayFormatter } from "../utils/weatherDataFormatter";
+
+const useCurrentWeather = () => {
+  const { cityName, lat, lon } = useAppSelector(selectLocation);
+
+  const {
+    data: rawData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["currentWeather", lat, lon, cityName],
+    queryFn: () =>
+      fetchCurrentDay({
+        cityName,
+        lat,
+        lon,
+      }),
+  });
+
+  const data: IWeather = rawData && weatherTodayFormatter(rawData);
+
+  return {
+    currentWeather: data,
+    currentWeatherIsLoading: isLoading,
+    currentWeatherIsError: isError,
+    currentWeatherError: error,
+  };
+};
+
+export default useCurrentWeather;
