673beeab0aba51fa65d3509d8a19612c7538787b
Fernando de Row <ferdr89dev@gmail.com>
2024-09-23 16:18:26 -0300

    feat: add userLocation hook

---
+++ src/lib/hooks/useLocation.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+import { ILocation } from "../interfaces/location";
+
+const useLocation = () => {
+  const [coords, setCoords] = useState<
+    Pick<ILocation, "lat" | "lon"> | undefined
+  >();
+
+  const success = ({
+    coords: { latitude, longitude },
+  }: {
+    coords: { latitude: number; longitude: number };
+  }) => {
+    setCoords({ lat: latitude, lon: longitude });
+  };
+
+  function error(err: unknown) {
+    console.error(err);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, error, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+
+  return { lon: coords?.lon, lat: coords?.lat };
+};
+
+export default useLocation;
