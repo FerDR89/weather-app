c421e98a482591624b25477a44e28de1c895e5cc
Fernando de Row <ferdr89dev@gmail.com>
2024-11-21 15:25:15 -0300

    chore: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@hooks": path.resolve(__dirname, "./src/lib/hooks"),
+      "@interfaces": path.resolve(__dirname, "./src/lib/interfaces"),
+      "@reducers": path.resolve(__dirname, "./src/lib/reducers"),
+      "@utils": path.resolve(__dirname, "./src/lib/utils"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@store": path.resolve(__dirname, "./src/store"),
+    },
+  },
   plugins: [react()],
-})
+});
