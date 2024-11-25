bde71f89622c3a636249a467c3a1f8370c381999
Fernando de Row <ferdr89dev@gmail.com>
2024-11-25 14:18:00 -0300

    chore: add barrel file in utils and fix imports in timestampToDate

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,7 @@
+export { default as capitalize } from "./capitalize";
+export { default as getWeekDay } from "./getWeekDay";
+export { default as timestampToDate } from "./timestampToDate";
+export {
+  weatherTodayFormatter,
+  weatherNextFiveFormatter,
+} from "./weatherDataFormatter";
--- src/lib/utils/timestampToDate.ts
+++ src/lib/utils/timestampToDate.ts
@@ -1,4 +1,4 @@
-import capitalize from "./capitalize";
+import { capitalize } from "@utils";
 
 const timestampToDate = (time: number): string => {
   const formattedDate = new Intl.DateTimeFormat("es-AR", {
