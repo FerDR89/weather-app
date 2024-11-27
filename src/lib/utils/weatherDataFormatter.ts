8a0f9ccc9d9dc1742f8b5b42da7a5d8013690e66
Fernando de Row <ferdr89dev@gmail.com>
2024-11-27 16:17:20 -0300

    refactor: remove some properties in weatherTodayFormatter, fix imports and improve weatherNextFiveDays

--- src/lib/utils/weatherDataFormatter.ts
+++ src/lib/utils/weatherDataFormatter.ts
@@ -1,13 +1,11 @@
 import {
   IWeather,
   IWeatherForecastList,
+  IWeatherNextFiveDays,
   IWeatherNextFiveResponse,
   IWeatherTodayResponse,
-} from "../interfaces/weather";
-import capitalize from "./capitalize";
-import getDate from "./getDate";
-import getWeekDay from "./getWeekDay";
-import timestampToDate from "./timestampToDate";
+} from "@interfaces";
+import { capitalize, getWeekDay, timestampToDate } from "@utils";
 
 export const weatherTodayFormatter = (
   data: IWeatherTodayResponse,
@@ -16,38 +14,33 @@ export const weatherTodayFormatter = (
   day: timestampToDate(data?.dt),
   temp: Math.ceil(data?.main["temp"]),
   feels_like: Math.ceil(data?.main["feels_like"]),
-  temp_min: Math.floor(data?.main["temp_min"]),
-  temp_max: Math.ceil(data?.main["temp_max"]),
-  humidity: Math.ceil(data?.main["humidity"]),
   description: capitalize(data?.weather[0]["description"]),
   icon: data?.weather[0]["icon"],
 });
 
-export const weatherNextFiveFormatter = (
-  data: IWeatherNextFiveResponse,
-): IWeather[] => {
-  // TODO: Antes de unificar todos los datos por día, calcular el promedio de temp_min y temp_max
+export const weatherNextFiveFormatter = (data: IWeatherNextFiveResponse) => {
+  const today = getWeekDay(new Date().getTime() / 1000);
 
-  const filterNextFiveDays = data.list.filter(
-    (t: IWeatherForecastList, i: number) => {
-      const currentDay = getDate(t.dt);
-      const nextDay = getDate(data.list[i + 1]?.dt);
-      return currentDay !== nextDay;
-    },
+  const mapedArr = data.list.map((t: IWeatherForecastList) => ({
+    day: today === getWeekDay(t?.dt) ? "Hoy" : getWeekDay(t?.dt),
+    temp_min: Math.ceil(t?.main["temp_min"]),
+    temp_max: Math.ceil(t?.main["temp_max"]),
+    icon: t?.weather[0]["icon"],
+  }));
+
+  const NextFiveDaysArray = Object.values(
+    mapedArr.reduce(
+      (acc, { day, temp_min, temp_max, icon }) => {
+        if (!acc[day]) {
+          acc[day] = { day, temp_min, temp_max, icon };
+        } else {
+          acc[day].temp_min = Math.min(acc[day].temp_min, temp_min);
+          acc[day].temp_max = Math.max(acc[day].temp_max, temp_max);
+        }
+        return acc;
+      },
+      {} as { [key: string]: IWeatherNextFiveDays },
+    ),
   );
-
-  // TODO: Usar slice en vez de shift para no mutar el array original.
-  filterNextFiveDays.shift();
-
-  const mappedNextFiveDays = filterNextFiveDays.map(
-    (t: IWeatherForecastList) => ({
-      day: getWeekDay(t?.dt),
-      temp: Math.ceil(t?.main["temp"]),
-      temp_min: Math.ceil(t?.main["temp_min"]),
-      temp_max: Math.ceil(t?.main["temp_max"]),
-      icon: t?.weather[0]["icon"],
-    }),
-  );
-
-  return mappedNextFiveDays;
+  return NextFiveDaysArray.slice(0, NextFiveDaysArray.length - 1);
 };
