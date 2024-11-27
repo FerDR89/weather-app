af4467b4d49b39192a4d9dc5e04e54c3bc538cad
Fernando de Row <ferdr89dev@gmail.com>
2024-11-27 16:05:09 -0300

    fix: add aliases in vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -5,5 +6,18 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/test/setup.ts",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@hooks": path.resolve(__dirname, "./src/lib/hooks"),
+      "@interfaces": path.resolve(__dirname, "./src/lib/interfaces"),
+      "@reducers": path.resolve(__dirname, "./src/lib/reducers"),
+      "@utils": path.resolve(__dirname, "./src/lib/utils"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@store": path.resolve(__dirname, "./src/store"),
+    },
   },
 });
